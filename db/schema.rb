e9ccd2b0b25f3fcd6b01f6c7d3dafdada1dfa962
Thomas Ramfjord <thomas.ramfjord@gmail.com>
2011-11-29 23:44:26 +1030

    added email index in users, with uniqueness enforced

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111129111901) do
+ActiveRecord::Schema.define(:version => 20111129131220) do
 
   create_table "problemanswers", :force => true do |t|
     t.boolean  "correct"
@@ -34,5 +34,7 @@ ActiveRecord::Schema.define(:version => 20111129111901) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
