283ff8e07fe91309f2f5dd02d46adc2123218951
Thomas Ramfjord <thomas.ramfjord@gmail.com>
2011-11-30 20:05:09 +1030

    migration for encrypted_password field

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111129131220) do
+ActiveRecord::Schema.define(:version => 20111130093302) do
 
   create_table "problemanswers", :force => true do |t|
     t.boolean  "correct"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(:version => 20111129131220) do
     t.string   "perms"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "encrypted_password"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
