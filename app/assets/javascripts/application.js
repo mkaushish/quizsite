0b8e5c3181ca53e1a85f685c6a89998c48784b4b
Thomas Ramfjord <thomas.ramfjord@gmail.com>
2011-12-06 17:32:46 +1030

    added multitextfield support

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,3 +7,21 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+$(document).ready(function () {
+  $('.add_mt_field').click(function() {
+    var num = $(this).siblings(".mt_field").length;
+    var new_field = $(this).attr('template').replace(/_num_/g, num);
+
+    $(this).siblings(".del_mt_field").attr("disabled","");
+    $(this).before(new_field);
+  });
+
+  $('.del_mt_field').click(function() {
+    var num = $(this).siblings(".mt_field").length;
+
+    if(num > 1) {
+      $(this).siblings(".mt_field").last().remove();
+    }
+  });
+});
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,4 +17,16 @@ module ApplicationHelper
     flash[:errors] ||= []
     flash[:errors]
   end
+
+  def add_multitext_field_link(qb_name, link_name = "+")
+    # data-partial same as in _multihtmlobj.html.erb
+    link_to link_name, '#',
+      :template => text_field_tag("#{qb_name}__num_", nil, :class => 'mt_field').gsub(/"/,"'").html_safe,
+      :class => "add_mt_field"
+      #, "data-partial" => text_field_tag("#{qb_name}__num_", nil, :class => 'mt_field'),
+  end
+
+  def del_multitext_field_link(link_name = "-")
+    link_to link_name, "#", :class => "del_mt_field"
+  end
 end
