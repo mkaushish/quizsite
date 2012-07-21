a0411d22f0f51e6096c63f440ae653a1a4bf361f
Thomas Ramfjord <thomas.ramfjord@gmail.com>
2012-07-21 05:49:31 +0530

    changed default quizzes to just assigning a class - my class

--- app/models/student.rb
+++ app/models/student.rb
@@ -15,8 +15,12 @@ class Student < User
 
   # Add a default quiz for each chapter
   def add_default_quizzes
-    CHAPTERS.each do |chapter|
-      practice_sets.create!(:problemtypes => Marshal.dump(chapter::PROBLEMS), :name => chapter.to_s)
-    end
+    #CHAPTERS.each do |chapter|
+    #  practice_sets.create!(:problemtypes => Marshal.dump(chapter::PROBLEMS), :name => chapter.to_s)
+    #end
+
+    # for now we'll assign them to my class instead, and then assign that class quizzes
+    classroom = User.find_by_email("t.homasramfjord@gmail.com").classrooms.first
+    classroom.assign!(self)
   end
 end
