d9b2c2a5bc58a890817aa3a0fd7851c12b8fa1a1
Thomas Ramfjord <thomas.ramfjord@gmail.com>
2013-04-08 13:26:10 +0530

    fixed explanation to use initProblem and closeWithDimmer

--- app/assets/javascripts/problem.js
+++ app/assets/javascripts/problem.js
@@ -3,16 +3,28 @@ function hideProblem() {
   $('#dimmer').hide();
 }
 
+function closeWithDimmer(overlay) {
+  var dimmer = $("#dimmer");
+  if(dimmer.length == 0) {
+    $('body').prepend("<div id=dimmer></div><div id=problem_overlay></div>");
+    dimmer = $("#dimmer");
+  }
+  else {
+    dimmer.unbind("click");
+  }
+
+  dimmer.show();
+  dimmer.click(hideProblem);
+}
+
 function initProblemOverlay() {
   var $p = $('#problem_overlay');
   // console.log("found " + $p);
   if($p.length == 0){
-   $('body').prepend("<div id=dimmer></div><div id=problem_overlay></div>");
    $p = $('#problem_overlay');
   }
 
   $p.show();
-  $("#dimmer").show();
-  $("#dimmer").click(hideProblem);
+  closeWithDimmer();
   return $p;
 }
