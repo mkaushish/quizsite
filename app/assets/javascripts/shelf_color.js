31e0157da7f9fc7bda418f1700994343a7a77c37
Madhav Kaushish <madhav.kaushish@gmail.com>
2013-06-18 19:16:50 +0530

    Readded cricket data again.. will kill github if it goes away

--- app/assets/javascripts/shelf_color.js
+++ app/assets/javascripts/shelf_color.js
@@ -37,5 +37,13 @@ else {
 }
 $('#bigshelf .shelf ul').css("background","-moz-linear-gradient(0% 0% 0deg,#0087bd, #1414C9, #0087bd "+blue_per+"%,#60b349 0%,#60b349 "+(red_per)+"%,#ff0040 0%)");
 
-
+}
+function shelf_color_in(blue, green, red){
+  if(green != "0"){
+$('#bigshelf .shelf ul').css("background","-webkit-gradient(linear,"+blue+" 0%,"+(red)+ " 0%, from(#0087bd), to(#ff0040), color-stop(.005,#60b349),color-stop(.995,#60b349))")
+}
+else{
+    $('#bigshelf .shelf ul').css("background","-webkit-linear-gradient(left,#0087bd "+blue+",#ff0040 "+blue); 
+}
+$('#bigshelf .shelf ul').css("background","-moz-linear-gradient(0% 0% 0deg,#0087bd, #1414C9, #0087bd "+blue+",#60b349 0%,#60b349 "+red+",#ff0040 0%)");
 }
