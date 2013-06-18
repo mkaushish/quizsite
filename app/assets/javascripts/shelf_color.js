121e41fc9c29e7f0cd9d85ac6f877fefd40a56bb
Madhav Kaushish <madhav.kaushish@gmail.com>
2013-06-18 09:10:47 +0530

    buttons, colors, look

--- app/assets/javascripts/shelf_color.js
+++ app/assets/javascripts/shelf_color.js
@@ -30,13 +30,12 @@ var red_per=  green_per + blue_per;
 
 //$('#bigshelf .shelf ul').css("background","-webkit-linear-gradient(left,#0087bd "+blue_per+"%,#ff0040 "+blue_per+"%)"); 
 if(green_per!=0){
-$('#bigshelf .shelf ul').css("background","-webkit-gradient(linear,"+blue_per+"% 0%,"+red_per+"% 0%, from(#0087bd), to(#ff0040), color-stop(.01,#60b349),color-stop(.99,#60b349))")
+$('#bigshelf .shelf ul').css("background","-webkit-gradient(linear,"+blue_per+"% 0%,"+red_per+"% 0%, from(#0087bd), to(#ff0040), color-stop(.005,#60b349),color-stop(.995,#60b349))");
 }
 else {
   $('#bigshelf .shelf ul').css("background","-webkit-linear-gradient(left,#0087bd "+blue_per+"%,#ff0040 "+blue_per+"%)"); 
-
-
 }
+$('#bigshelf .shelf ul').css("background","-moz-linear-gradient(0% 0% 0deg,#0087bd, #1414C9, #0087bd "+blue_per+"%,#60b349 0%,#60b349 "+(red_per)+"%,#ff0040 0%)");
 
 
 }
