d3411cdc8d15e5c3f2b1e2f6ca547446672e0d52
Amandeep <amandeep.bhamra@gmail.com>
2013-07-02 18:46:42 +0530

    added bagdes, their levels and points

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -2,7 +2,15 @@ class Badge < ActiveRecord::Base
   	# attr_accessible :title, :body
   	belongs_to :student
 
-	# Badge for all problem sets done #
+  	# LEVELS  => POINTS #
+  	# 1       => 100    # 
+  	# 2		  => 1000	#
+  	# 3		  => 5000   #
+  	# 4		  => 20000	#
+  	# 5 	  => 50000  #
+
+
+	# Badge for all problem sets done [LEVEL 5]#
 	def self.BadgeAPSD(student)
         @result = Array.new
         student.problem_set_instances.order("id").each do |pset|
@@ -12,33 +20,45 @@ class Badge < ActiveRecord::Base
         true_count = @result.select {|v| v =="true"}.count
         if (result_length- true_count) == 0
         	@has_BadgeAPSD = student.badges.find_by_badge_key("BadgeAPSD") 
-        	@has_BadgeAPSD ||= student.badges.create(:name => "All problem sets done", :badge_key => "BadgeAPSD")
+        	if @has_BadgeAPSD.nil?
+        		student.points += 50000
+        		student.save
+        		student.badges.create(:name => "All problem sets done", :badge_key => "BadgeAPSD", :level => 5)
+        	end
 		end
     end
 
-	# Badge for getting problem set blue #
+	# Badge for getting problem set blue [LEVEL 3] #
 	def self.BadgePSB(student)
 		student.problem_set_instances.order("id").each do |pset|
             if pset.num_problems - pset.problem_stats.blue.count == 0
             	pset_name = pset.problem_set.name
             	@has_BadgePSB = student.badges.find_by_badge_key("BadgePSB")
-            	@has_BadgePSB ||=  student.badges.create(:name => "#{pset_name} Blue !!", :badge_key => "BadgePSB")	
+            	if @has_BadgePSB.nil?
+            		student.points += 5000
+            		student.save
+            		student.badges.create(:name => "#{pset_name} Blue !!", :badge_key => "BadgePSB", :level => 3)
+            	end
         	end
         end
     end
 
-    # Badge for getting problem type blue #
+    # Badge for getting problem type blue [LEVEL 1]#
     def self.BadgePTB(student)
     	student.problem_set_instances.order("id").each do |pset|
     		pset.problem_stats.blue.map(&:problem_type_id).each do |ptype|
     			problem_type = ProblemType.find_by_id(ptype)
     			@has_BadgePTB = student.badges.find_by_badge_key("Badge#{problem_type.name}B")
-            	@has_BadgePTB = student.badges.create(:name => "#{problem_type.name} Blue !!", :badge_key => "Badge#{problem_type.name}B") if @has_BadgePTB.nil?
-            end
+            	if @has_BadgePTB.nil?
+            		student.points += 100
+            		student.save
+            		student.badges.create(:name => "#{problem_type.name} Blue !!", :badge_key => "Badge#{problem_type.name}B", :level => 1)
+            	end	
+        	end
         end
     end
 
-    # Badge for getting n problem sets blue #
+    # Badge for getting n problem sets blue [LEVEL 4]#
     def self.BadgeNPSB(student, n)
     	@result = Array.new
         student.problem_set_instances.order("id").each do |pset|
@@ -48,11 +68,15 @@ class Badge < ActiveRecord::Base
         true_count = @result.select {|v| v =="true"}.count
         if true_count == n
         	@has_BadgeNPSD = student.badges.find_by_badge_key("Badge#{n}PSD") 
-        	@has_BadgeNPSD = student.badges.create(:name => "#{n} problem sets done", :badge_key => "Badge#{n}PSD") if @has_BadgeNPSD.nil?
+        	if @has_BadgeNPSD.nil?
+        		student.points += 20000
+        		student.save
+        		student.badges.create(:name => "#{n} problem sets done", :badge_key => "Badge#{n}PSD", :level => 4)
+        	end
 		end
     end
 
-	# Badge for n questions correct in a row for the first time only #
+	# Badge for n questions correct in a row for the first time only [LEVEL 2]#
 	def self.BadgeNQCIARFTO(student, n)
 		@b = student.answers.order("created_at DESC").limit(n).map(&:correct)
 		
@@ -61,36 +85,46 @@ class Badge < ActiveRecord::Base
 			result = @b.length - @a
 			if result == 0
 				@has_BadgeNQCIARFTO = student.badges.find_by_badge_key("Badge#{n}QCIARFTO")
-				@has_BadgeNQCIARFTO ||= student.badges.create(:name => "#{n} questions correct in a row for the first time only",
-																:badge_key => "Badge#{n}QCIARFTO") if @has_BadgeNQCIARFTO.nil?
+				if @has_BadgeNQCIARFTO.nil?
+					student.points += 1000
+					student.save
+					student.badges.create(:name => "#{n} questions correct in a row for the first time only", :badge_key => "Badge#{n}QCIARFTO", :level => 2)
+				end					
 			end
 		end
 	end
 
-	# Badge for completing a problem set within a day #
+	# Badge for completing a problem set within a day [LEVEL 2]#
 	def self.BadgeCAPSWAD(student)
 		@result = student.problem_set_instances.includes(&:problem_stats).map(&:stop_green)
 		unless @result.blank?
 			if @result.select{|v| v == Date.today}.count > 0 == true
 				@has_BadgeCAPSWAD = student.badges.find_by_badge_key("BadgeCAPSWAD")
-				@has_BadgeCAPSWAD ||= student.badges.create(:name => "Completing a problem set within a day",
-																:badge_key => "BadgeCAPSWAD") if @has_BadgeCAPSWAD.nil?
+				if @has_BadgeCAPSWAD.nil?
+					student.points += 1000
+					student.save
+					student.badges.create(:name => "Completing a problem set within a day",	:badge_key => "BadgeCAPSWAD")
+				end					
 			end
 		end	
 	end
 
-	# Badge for getting first 10 red questions correct #
+	# Badge for getting first 10 red questions correct [LEVEL 1]#
 	def self.BadgeTRQC(student)
 		@result = student.answers.order("created_at DESC").where(:correct => true).count == 10
 		unless @result.blank?
 			if @result == true
 				@has_BadgeTRQC = student.badges.find_by_badge_key("BadgeTRQC")
-				@has_BadgeTRQC ||= student.badges.create(:name => "First 10 red questions correct",
-																:badge_key => "BadgeTRQC") if @has_BadgeTRQC.nil?
+				if @has_BadgeTRQC.nil?
+					student.points += 100
+					student.save
+					student.badges.create(:name => "First 10 red questions correct", :badge_key => "BadgeTRQC")
+				end					
 			end
 		end
 	end
 
+	# Badge for getting n questions correct in a row for n times [LEVEL 3] #
 	def self.BadgeNQCIARFNT(student, n, times)
 		a = 0
 		b = 0
@@ -106,7 +140,11 @@ class Badge < ActiveRecord::Base
 			end
           	if b == times
 	        	@has_BadgeNQCIARFNT = student.badges.find_by_badge_key("Badge#{n}QCIARF#{times}T") 
-    	    	@has_BadgeNQCIARFNT = student.badges.create(:name => "#{n} questions correction in a row #{times} times", :badge_key => "Badge#{n}QCIARF#{times}T") if @has_BadgeNQCIARFNT.nil?
+    	    	if @has_BadgeNQCIARFNT.nil?
+    	    		student.points += 5000
+    	    		student.save
+    	    		student.badges.create(:name => "#{n} questions correction in a row #{times} times", :badge_key => "Badge#{n}QCIARF#{times}T", :level => 3)
+          		end	
           	end
 		end
 	end
