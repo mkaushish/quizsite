e51c5c3c9678dbf8373abe77d844d0fb2bfaaddb
Amandeep <amandeep.bhamra@gmail.com>
2013-07-10 14:07:18 +0530

    levels added to badges

--- db/migrate/20130617081504_add_columns_to_problem_sets.rb
+++
@@ -1,7 +0,0 @@
-class AddColumnsToProblemSets < ActiveRecord::Migration
-	def change
-		add_column :problem_sets, :image, :string
-		add_column :problem_sets, :description, :string
-		add_column :problem_sets, :video_link, :string
-	end
-end
