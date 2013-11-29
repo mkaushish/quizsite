fb3ab958e6063411bef953d42abaf1a6c415f358
Amandeep Singh <amandeep.bhamra@gmail.com>
2013-11-29 11:49:43 +0530

    no clue

--- db/migrate/20130910101543_create_topics.rb
+++ db/migrate/20130910101543_create_topics.rb
@@ -2,10 +2,11 @@ class CreateTopics < ActiveRecord::Migration
   	def change
     	create_table :topics do |t|
 			t.string :title
+			t.text	:description
 			t.integer :user_id
 			t.integer :classroom_id
 			t.integer :comments_count, :default => 0
-	
+			
 			t.timestamps
     	end
   	end
